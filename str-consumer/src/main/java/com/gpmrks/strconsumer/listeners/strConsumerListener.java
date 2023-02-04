6ea44079b361427104f82356e84ae2b5f0c5c842
Guilherme Marques <gpmrks@GPMrks>
2023-02-04 15:34:08 -0300

    FEAT - Developed custom annotations to StrConsumerListener in order to avoid code repetition

--- str-consumer/src/main/java/com/gpmrks/strconsumer/listeners/strConsumerListener.java
+++ str-consumer/src/main/java/com/gpmrks/strconsumer/listeners/strConsumerListener.java
@@ -1,15 +1,27 @@
 package com.gpmrks.strconsumer.listeners;
 
+import com.gpmrks.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 @Log4j2
 public class strConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Message received {}", message);
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Message received {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Message received {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Message received {}", message);
     }
 }
